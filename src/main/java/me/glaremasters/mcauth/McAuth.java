3148074f081a5486a4b34de5369e5a0b00ab4e14
darbyjack <admin@glaremasters.me>
2019-06-18 17:41:46 -0500

    Initial Commit

---
+++ src/main/java/me/glaremasters/mcauth/McAuth.java
@@ -0,0 +1,17 @@
+package me.glaremasters.mcauth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McAuth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
